405f5c3f13520734b247ac057151dcdae75fb75a
Zervanto <1019335509@qq.com>
2017-05-07 20:32:03 +0800

    update keyboardevent

--- keyboard.js
+++ keyboard.js
@@ -2,11 +2,9 @@
 * @Author: Zervanto
 * @Date:   2017-05-05 17:30:32
 * @Last Modified by:   Zervanto
-* @Last Modified time: 2017-05-05 18:08:30
+* @Last Modified time: 2017-05-07 20:30:09
 */
-
-'use strict';
-var KeyboardEventComp = React.createClass({
+var Comp = React.createClass({
     getInitialState(){
         return{
             top:0,
@@ -18,34 +16,33 @@ var KeyboardEventComp = React.createClass({
             case 37:
                 this.setState({left:this.state.left-5});break;
             case 38:
-                this.setState({top:this.state.top+5});break;
+                this.setState({top:this.state.top-5});break;
             case 39:
                 this.setState({left:this.state.left+5});break;
             case 40:
-                this.setState({top:this.state.top-5});break;
+                this.setState({top:this.state.top+5});break;
         }
     },
     render(){
-        return(
-            < div style={
-            {width:"450px",
+
+        return (
+            <div style={{width:"450px",
             height:"450px",
             position:"relative",
-            background:yellow}
-        }  onkeyUp={this.handlekeyup}>
-            < div style={
+            background:"yellow"}} tabIndex={1} onKeyUp={this.handlekeyup}>
+                < div style={
                 {position:'absolute',
                 width:"10px",
                 height:"10px",
                 top:this.state.top+'px',
                 left:this.state.left+'px',
-                background:red}}>
+                background:"red"}}>
+                </div>
             </div>
-        </div>)
-
+            );
     }
 });
 ReactDOM.render(
-    < KeyboardEventComp />,
+    <Comp/>,
     document.getElementById('app')
-    );
+);
