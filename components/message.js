8082f09a7bbf77ed73067d38a21f126b30bbbb6b
Zervanto <1019335509@qq.com>
2017-05-09 11:31:49 +0800

    update message.js

--- components/message.js
+++ components/message.js
@@ -2,9 +2,9 @@
 * @Author: Zervanto
 * @Date:   2017-05-08 11:08:31
 * @Last Modified by:   Zervanto
-* @Last Modified time: 2017-05-08 16:14:02
+* @Last Modified time: 2017-05-09 11:29:05
 */
-const list =[
+const listarr =[
     "AAA",
     "BBB",
     "CCC",
@@ -13,18 +13,28 @@ const list =[
 const Item = React.createClass({
     render(){
         let style = this.props.actived?{border:"1px solid green"}:{};
-        return <li style={style}>{this.props.item}</li>
+        return <li onClick={this.props.callback} style={style}>{this.props.name}</li>
     }
 });
 const Comp = React.createClass({
     getInitialState(){
         return {
-            list:[]
+            list:{}
         }
     },
-    componentWillMount(){
-        this.state.list = this.props.data.map(item=>{return{name:item,actived:false}});
+    callback(item){
+        alert(item.name)
     },
+    componentWillMount(){
+        this.state.list = this.props.data.map(item =>{
+            return { name:item,actived:false}
+        });
+    },
+    // componentWillMount(){
+    //     this.state.list = this.props.data.map(function(item){
+    //         return { name:item,actived:false}
+    //     });
+    // },
     componentDidMount(){
         setTimeout(()=>{
             this.state.list[1].actived = true;
@@ -32,9 +42,10 @@ const Comp = React.createClass({
         },3000);
     },
     render(){
-        return <ul>{this.props.data.map(item =>{return <Item actived={item.actived} name={item.name}/>})}</ul>
+        return <ul>{this.state.list.map(
+            item => <Item callback={this.callback.bind(this,item)} actived={item.actived} name={item.name}/>)}</ul>
     }
 });
 ReactDOM.render(
-    <Comp data={list}/>,document.body
+    <Comp data={listarr}/>,document.body
     );
