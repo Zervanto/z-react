b5e4c07a751d48439c263f57a215e6d91a0b0169
Zervanto <1019335509@qq.com>
2017-05-10 23:02:19 +0800

    update npm envir

---
+++ src/jsx/purefunction.js
@@ -0,0 +1,31 @@
+function pureFn(state){
+    //1.创建一个新state
+    let num = ++state.num;
+    return {
+        num
+    }
+    //2.返回原来的state
+    // return state
+}
+function store(){
+    let state = {
+        aaa:{name:'Zervanto'},
+        bbb:{group:'javascript'},
+        ccc:{age:23}
+    }
+    const keys = Object.keys(updaters);
+    keys.forEach(key=>{
+        let updater= updaters[key];
+        let value = state[key];
+        let newSubState[key];
+        newState[key]=newSubState;
+    });
+    Object.assign({},state,newState);
+    console.log(newState);
+}
+function aaaUpdater(substate){
+
+}
+function bbbUpdater(substate){
+
+}
